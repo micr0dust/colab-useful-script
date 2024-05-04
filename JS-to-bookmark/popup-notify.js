83c3239f53ab9662ad2510db9b9cd3cac768170c
micr0dust <wuilliamolgo104286@gmail.com>
2024-05-04 16:50:03 +0800

    comment

--- JS-to-bookmark/popup-notify.js
+++ JS-to-bookmark/popup-notify.js
@@ -1,1 +1,3 @@
 javascript:var popupCheckInterval=setInterval((function(){let e=document.querySelector("body > mwc-dialog");if(e){let t=window.getComputedStyle(e);"none"!==t.display&&"hidden"!==t.visibility&&(notify("https://github.com/micr0dust/colab-useful-script/raw/main/sound/You%20have%20been%20defeated.mp3?raw=true"),clearInterval(popupCheckInterval))}}),1e3);function notify(e){var t=document.createElement("audio");t.src=e,t.autoplay=!0,t.loop=!1,document.body.appendChild(t)}
+
+// 彈窗偵測並聲音提醒 - 不包含機器人驗證
--- JS-to-bookmark/reconnect.js
+++ JS-to-bookmark/reconnect.js
@@ -1,1 +1,3 @@
 javascript:function ConnectButton(){document.querySelector("#top-toolbar > colab-connect-button").shadowRoot.querySelector("#connect").click()}setInterval(ConnectButton,6e4);
+
+// colab 中斷重連
